9acd5877ac2abe150d59ca16b7b0401fefb60e58
Vndanilchenko <44552914+Vndanilchenko@users.noreply.github.com>
2018-10-28 22:29:01 +0300

    create hw1.sql
    домашнее задание 2

---
+++ hw1.sql
@@ -0,0 +1,54 @@
+SELECT ('ФИО: Данильченко Вадим');
+-------------------- Домашнее заданиче №2 ------------------------
+--------------------- Данильченко Вадим --------------------------
+
+--1. 10 первых записей
+select * 
+from ratings 
+limit 10;
+
+select * 
+from links 
+where imdbid like '%42' and movieid between 100 and 1000
+limit 10;
+
+--2. JOIN
+select imdbid 
+from links l inner join ratings r 
+  on l.movieid=r.movieid where rating=5
+limit 10;
+
+
+--3. Агрегация
+select count(movieid) 
+from ratings 
+where rating is null
+limit 10;
+
+select userid 
+from ratings 
+group by userid 
+having avg(rating)>3.5 
+limit 10;
+
+--4. Иерархические запросы
+select imdbid 
+from links l inner join ratings r 
+  on l.movieid=r.movieid 
+group by l.imdbid 
+having avg(r.rating)>3.5 
+limit 10;
+
+with users as (
+  select userid 
+  from ratings 
+  group by userid 
+  having count(*)>10) 
+select userid, avg(rating) 
+from ratings 
+where userid in (
+  select * from users) 
+group by userid 
+limit 10;
+
+
